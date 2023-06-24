145379adff2c7bea57419268551eb7a55ef72390
Ondrej Jedinak <ondrej.jedinak@gmail.com>
2023-06-24 08:59:37 +0200

    get data from external API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import express, { Request, Response, Express } from 'express';
 import dotenv from 'dotenv';
+import { Product } from './types/types';
+
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+
+    let productIds = ['product-1', 'product-2']
+
+    let products: Product[] = []
+
+    for (let id of productIds){
+        
+        const result = await fetch(`https://dev.aux.boxpi.com/case-study/products/${id}/positions`, {
+            headers: {
+                'x-api-key': 'MVGBMS0VQI555bTery9qJ91BfUpi53N24SkKMf9Z',
+                'Content-Type': 'application/json'
+            }
+        })
+        
+        const data: Product[] = await result.json()
+        products.push(...data)
+    }
+
+    console.log(products)
+
     res.send({"Products": ['hi', 'me'], "distance": 0})
 })
 
